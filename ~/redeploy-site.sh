a3e00b11a85e87b8ac2114c294bdb0289342a43e
Ruchika Biswas <rbiswas4@illinois.edu>
2022-08-02 16:25:20 -0700

    added redeploy-site.sh

---
+++ ~/redeploy-site.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd project-shiny_sharks
+git fetch && git reset origin/main --hard
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+systemctl daemon-reload
+systemctl restart myportfolio
