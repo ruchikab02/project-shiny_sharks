5232ce48356e6ee743bd5adbe0aa07db4fef149e
ruchikab02 <73154491+ruchikab02@users.noreply.github.com>
2022-08-08 17:40:35 -0700

    Delete curl-test.sh

--- curl-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl --request POST http://localhost:5000/api/timeline_post -d 'name=Ruchika&email=ruchika.bee@gmail.com&content=Just Added Database to my portfolio site!'
-curl http://localhost:5000/api/timeline_post
